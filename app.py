fa6841358e0ad5d529509415d5c351ed3a979068
Alfred Essa <alfred@malpasocodes.com>
2024-09-15 15:53:21 -0400

    made change

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+# make a change
+
+
+def main():
+    st.title("Hello World")
+    st.write("This is a simple example of a Streamlit app.")
+
+    # Load data
+    data = pd.read_csv("data/accessandmobility.csv")
+    df = data.head(10)
+    st.write(data)
+st.write(df)
+
+
+if __name__ == "__main__":
+    main()
